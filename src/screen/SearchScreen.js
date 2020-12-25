348583bccbaeac68f3453ea70f8b1f7477229169
parth <parthkanani8490@gmail.com>
2020-12-25 15:18:19 +0530

    call api with axios

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SerachBar from "../components/SerachBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setterm] = useState("");
+  const [results, setresults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term: term,
+        location: "san jose",
+      },
+    });
+    setresults(response.data.businesses);
+  };
+
   return (
     <View>
       <SerachBar
         term={term}
         onChangeTerm={(newterm) => setterm(newterm)}
-        onTermSubmit={() => console.log("term submitted")}
+        onTermSubmit={() => searchApi()}
       />
       <Text>This is Search Screen</Text>
       <Text>{term}</Text>
+      <Text>we found {results.length} results</Text>
     </View>
   );
 };
