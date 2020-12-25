eb41440be5cda8de1d421727c9b225c63d0a5546
parth <parthkanani8490@gmail.com>
2020-12-25 11:21:46 +0530

    install navigation dependcys and create search screen

---
+++ src/screen/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>This is Search Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
