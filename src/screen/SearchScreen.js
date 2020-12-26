e296c22f691bc04bde1ffe2d7e0ef7059b234b69
parth <parthkanani8490@gmail.com>
2020-12-26 13:41:21 +0530

    featch data from api and disply in flatlist

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SerachBar from "../components/SerachBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setterm] = useState("");
   const [errorMessage, results, searchApi] = useResults();
+
+  const filterResultByPrice = (price) => {
+    return results.filter((results) => {
+      return results.price === price;
+    });
+  };
 
   return (
     <View>
@@ -16,6 +23,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>we found {results.length} results</Text>
+      <ResultsList title="Cost Effective" results={filterResultByPrice("$")} />
+      <ResultsList title="Big Pricier" results={filterResultByPrice("$$")} />
+      <ResultsList title="Big Spender" results={filterResultByPrice("$$$")} />
     </View>
   );
 };
